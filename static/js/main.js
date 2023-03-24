47b5b27fa67001935b378e31bfaa4648b034a2b2
Juan Oviedo <juan.oviedo@GEP.DC>
2023-03-24 08:58:59 -0500

    Terminado JS

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,12 @@
 const asamblea = document.querySelector("#asamblea");
 const ulAsamblea = document.querySelector("#ulAsamblea");
+
+asamblea.addEventListener('click', () => {
+    show(ulAsamblea);
+});
+
+/* Gerente */
+
 const gerente = document.querySelector("#gerente");
 const ulGerente = document.querySelector("#ulGerente");
 const dirJuridico = document.querySelector("#dirJuridico");
@@ -7,11 +14,10 @@ const ulDirJuridico = document.querySelector("#ulDirJuridico");
 const dirComercial = document.querySelector("#dirComercial");
 const ulDirComercial = document.querySelector("#ulDirComercial");
 const supTelemercadeo = document.querySelector("#supTelemercadeo");
-const ulSupTelemercadeo = document.querySelector("#ulSupTelemercadeoulSupTelemercadeo");
+const ulSupTelemercadeo = document.querySelector("#ulSupTelemercadeo");
+const adminSistemas = document.querySelector("#adminSistemas");
+const ulAdminSistemas = document.querySelector("#ulAdminSistemas");
 
-asamblea.addEventListener('click', () => {
-    show(ulAsamblea);
-});
 gerente.addEventListener('click', () => {
     show(ulGerente);
 });
@@ -24,6 +30,68 @@ dirComercial.addEventListener('click', () => {
 supTelemercadeo.addEventListener('click', () => {
     show(ulSupTelemercadeo);
 });
+adminSistemas.addEventListener('click', () => {
+    show(ulAdminSistemas);
+});
+
+/* End Gerente */
+
+/* RRHH */
+
+const jefeRRHH = document.querySelector("#jefeRRHH");
+const ulJefeRRHH = document.querySelector("#ulJefeRRHH");
+
+jefeRRHH.addEventListener('click', () => {
+    show(ulJefeRRHH);
+});
+
+/* End RRHH */
+
+/* subgerente */
+
+const subgerente = document.querySelector("#subgerente");
+const ulSubgerente = document.querySelector("#ulSubgerente");
+const coorSac = document.querySelector("#coorSac");
+const ulCoorSac = document.querySelector("#ulCoorSac");
+const superSCC = document.querySelector("#superSCC");
+const ulSuperSCC = document.querySelector("#ulSuperSCC");
+const areaAdmin = document.querySelector("#areaAdmin");
+const ulAreaAdmin = document.querySelector("#ulAreaAdmin");
+const coorCartera = document.querySelector("#coorCartera");
+const ulCoorCartera = document.querySelector("#ulCoorCartera");
+const superPQR = document.querySelector("#superPQR");
+const ulSuperPQR = document.querySelector("#ulSuperPQR");
+const contabilidad = document.querySelector("#contabilidad");
+const ulContabilidad = document.querySelector("#ulContabilidad");
+const psicologia = document.querySelector("#psicologia");
+const ulPsicologia = document.querySelector("#ulPsicologia");
+
+subgerente.addEventListener('click', () => {
+    show(ulSubgerente);
+});
+coorSac.addEventListener('click', () => {
+    show(ulCoorSac);
+});
+superSCC.addEventListener('click', () => {
+    show(ulSuperSCC);
+});
+areaAdmin.addEventListener('click', () => {
+    show(ulAreaAdmin);
+});
+coorCartera.addEventListener('click', () => {
+    show(ulCoorCartera);
+});
+superPQR.addEventListener('click', () => {
+    show(ulSuperPQR);
+});
+contabilidad.addEventListener('click', () => {
+    show(ulContabilidad);
+});
+psicologia.addEventListener('click', () => {
+    show(ulPsicologia);
+});
+
+/* End Subgerente */
 
 function show(element) {
     element.classList.toggle('hidden');
